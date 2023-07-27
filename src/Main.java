ba7ace21d8d2e2cfc74c9b73423458933d825a30
Vladimyr Buzhatskiy <buzhatskiy@gmail.com>
2023-07-27 18:11:33 +0300

    first task

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Method;
+class Pow {
+    @Test(a = 5, b = 4)
+    public static void test(int a, int b){
+        System.out.println("a^b = " + Math.pow(a, b));
+    }
+}
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException {
+        try {
+            Class<?> cls =  Pow.class;
+            Method method = cls.getMethod("test", int.class, int.class);
+            if(method.isAnnotationPresent(Test.class)){
+                Test abs = method.getAnnotation(Test.class);
+                method.invoke(null, abs.a(), abs.b());
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+    }
+}
